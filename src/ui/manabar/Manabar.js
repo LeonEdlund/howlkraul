e53b683ad6707fc7e3837f2f150a09fb5ffd9163
Leon Edlund <Leone.edlund@gmail.com>
2025-05-08 09:22:26 +0200

    seperate manabar from wizard class and remove playablecharacter as base class

--- src/ui/manabar/Manabar.js
+++ src/ui/manabar/Manabar.js
@@ -1,8 +1,10 @@
 howlkraul.ui.Manabar = function (character) {
+  this.m_character = character;
+
   //--------------------------------------------------------------------------
   // Super Call
   //--------------------------------------------------------------------------
-  rune.ui.Progressbar.call(this, x, y, 11, 12, "hearts_11x12");
+  rune.ui.Progressbar.call(this, this.m_character.width || 20, 2, "#cad4de", "#6697c4");
 }
 
 //--------------------------------------------------------------------------
@@ -23,19 +25,7 @@ howlkraul.ui.Manabar.prototype.constructor = howlkraul.ui.Manabar;
  * @returns {undefined}
  */
 howlkraul.ui.Manabar.prototype.setHeart = function (state) {
-  if (typeof state !== 'number') throw new Error('State needs to be a number');
 
-  switch (state) {
-    case 2:
-      this.animation.goto("full");
-      break;
-    case 1:
-      this.animation.goto("half");
-      break;
-    case 0:
-      this.animation.goto("empty");
-      break;
-  }
 };
 
 //--------------------------------------------------------------------------
@@ -46,23 +36,57 @@ howlkraul.ui.Manabar.prototype.setHeart = function (state) {
  * @inheritdoc
  */
 howlkraul.ui.Manabar.prototype.init = function () {
-  rune.display.Sprite.prototype.init.call(this);
-  this.m_initFrames();
+  rune.ui.Progressbar.prototype.init.call(this);
+
+  this.progress = this.m_character.energy / 100;
+};
+
+/**
+ * @inheritdoc
+ */
+howlkraul.ui.Manabar.prototype.update = function (step) {
+  rune.ui.Progressbar.prototype.init.call(this, step);
+
+  this.m_followPlayer();
+  this.m_handleEmptyMana();
+  this.m_regenBar()
+};
+
+/**
+ * @inheritdoc
+ */
+howlkraul.ui.Manabar.prototype.dispose = function () {
+  rune.ui.Progressbar.prototype.dispose.call(this);
 };
 
 //--------------------------------------------------------------------------
 // Private methods
 //--------------------------------------------------------------------------
 
+
 /**
- * Initiates the frames.
- * 
- * @private
- * @returns {undefined} 
+ * @inheritdoc
  */
-howlkraul.ui.Manabar.prototype.m_initFrames = function () {
-  this.animation.create("full", [2], 0, false);
-  this.animation.create("half", [1], 0, false);
-  this.animation.create("empty", [0], 0, false);
-  this.animation.goto("full");
+howlkraul.ui.Manabar.prototype.m_followPlayer = function () {
+  if (this.m_character) {
+    this.moveTo(this.m_character.bottomLeft.x, this.m_character.bottomLeft.y + 2);
+  }
 };
+
+howlkraul.ui.Manabar.prototype.m_handleEmptyMana = function () {
+  if (this.m_character && this.m_character.m_energy <= 0) {
+    this.forgroundColor = "red";
+  }
+}
+
+howlkraul.ui.Manabar.prototype.m_regenBar = function () {
+  if (this.m_character) {
+    this.progress = this.m_character.energy / 100;
+
+    if (this.m_character.energy >= 100) {
+      if (this.forgroundColor !== "#6697c4") {
+        this.forgroundColor = "#6697c4";
+      }
+    }
+  }
+}
