6af98027a7f942edf3fd3720ab98d340ea8d9f05
Theo Myrvold <theomyrvold4@hotmail.com>
2025-05-20 14:15:35 +0200

    start mennu icons

--- src/data/resource/Requests.js
+++ src/data/resource/Requests.js
@@ -86,10 +86,12 @@ howlkraul.data.Requests.prototype.m_construct = function() {
 	this.add("coin_hud_12x18", "./../asset/img/hud/hud components/coin_hud_12x18.png");
 	this.add("player_selection_p1_37x19", "./../asset/img/hud/selectors/player_selection_p1_37x19.png");
 	this.add("player_selection_p2_37x19", "./../asset/img/hud/selectors/player_selection_p2_37x19.png");
+	this.add("controller_30x31", "./../asset/img/hud/start screen/controller_30x31.png");
 	this.add("controllerl_302x132", "./../asset/img/hud/start screen/controllerl_302x132.png");
 	this.add("highscoreV1_167x182", "./../asset/img/hud/start screen/highscoreV1_167x182.png");
 	this.add("highscore_93x120", "./../asset/img/hud/start screen/highscore_93x120.png");
 	this.add("howlkraul_314x82", "./../asset/img/hud/start screen/howlkraul_314x82.png");
+	this.add("info_30x31", "./../asset/img/hud/start screen/info_30x31.png");
 	this.add("tutorial_text_114x61", "./../asset/img/hud/start screen/tutorial_text_114x61.png");
 	this.add("xfragmentation1_6x8", "./../asset/img/not_in_use/bomb/xfragmentation1_6x8.png");
 	this.add("xfragmentation2_6x8", "./../asset/img/not_in_use/bomb/xfragmentation2_6x8.png");
